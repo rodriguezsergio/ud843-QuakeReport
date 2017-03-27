cb515f2fa54525c261aa739bd1e02bba7cb4a31c
Sergio <sergio@compstak.com>
2017-03-26 22:11:23 -0400

    Complete quiz: Parse JSON Response In Quake Report App

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -31,14 +31,16 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<Earthquake>();
+        /*ArrayList<Earthquake> earthquakes = new ArrayList<Earthquake>();
         earthquakes.add(new Earthquake("San Francisco", "7.2", "Feb 2, 2016"));
         earthquakes.add(new Earthquake("London", "6.1", "July 20, 2015"));
         earthquakes.add(new Earthquake("Tokyo", "3.9", "Nov 10, 2014"));
         earthquakes.add(new Earthquake("Mexico City", "5.4", "May 3, 2014"));
         earthquakes.add(new Earthquake("Moscow", "2.8", "Jan 31, 2013"));
         earthquakes.add(new Earthquake("Rio de Janeiro", "4.9", "Aug 19, 2012"));
-        earthquakes.add(new Earthquake("Paris", "1.6", "Oct 30, 2011"));
+        earthquakes.add(new Earthquake("Paris", "1.6", "Oct 30, 2011"));*/
+
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         QuakeAdapter itemsAdapter = new QuakeAdapter(this, earthquakes);
 
